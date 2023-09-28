7328467569df569d9dabcaf2cb05fa7eefab2b5c
k0zlovn1kita <144094179+k0zlovn1kita@users.noreply.github.com>
2023-09-28 11:32:04 +0300

    Add files via upload

---
+++ FileName.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <stdlib.h>
+#include <windows.h>
+
+using namespace std;
+
+
+int main()
+
+{
+
+
+    cout << " HELLO !!!\n";
+
+
+
+    cout << endl << endl << endl << endl << endl;
+    cout << endl << endl << endl << endl << endl;
+    cout << " ------------------" << endl;
+    cout << " |                |" << endl;
+    cout << " |                |" << endl;
+    cout << " |                |" << endl;
+    cout << " |                |" << endl;
+    cout << " |                |" << endl;
+    cout << " |                |" << endl;
+    cout << " |                |" << endl;
+    cout << " ------------------" << endl;
+
+    cout << "  * * " << endl;
+    cout << " *   *" << endl;
+    cout << " *   *" << endl;
+    cout << "  * * " << endl;
+    
+
+
+
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    cout << "       Прізвище : Козлов" << endl;
+    cout << "           Ім'я :  Нікіта" << endl;
+    cout << "          Стать : чоловіча" << endl;
+    cout << "Дата народження : 2006-05-17" << endl;
+    cout << "         Адреса : Дніпро вул.Миру 8" << endl;
+    cout << " Номер телефону : +380 99 003 0986" << endl;
+    cout << " Місце навчання : ФПМ, ДНУ ім. Олеся Гончара" << endl;
+    cout << "         Група  : ПА-23-1" << endl;
+    cout << "          Хоббі : музика" << endl;
+    system("pause");
+}
